b0f4aa4777e69ee91956ce614388fd74fa07fec4
ChimaG <chimagee@mymusictaste.com>
2021-06-07 10:05:28 +0800

    fix: show alert when upload complete

--- src/services/upload.service.ts
+++ src/services/upload.service.ts
@@ -1,12 +1,12 @@
 import http from "./http.service";
 import AWS from "aws-sdk";
-const S3_BUCKET_NAME = process.env.AWS_BUCKET_NAME || "delivuspwatest";
+const S3_BUCKET_NAME = process.env.AWS_BUCKET_NAME || "delivuspwa";
 const AWS_REGION = process.env.AWS_REGION || "ap-northeast-2";
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_ID || "AKIAQNNRC5IFRWJJVN7H",
+  accessKeyId: process.env.AWS_ACCESS_ID || "AKIARBBRIOAM6BIKEVS2",
   secretAccessKey:
-    process.env.AWS_SECRET_KEY || "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",
+    process.env.AWS_SECRET_KEY || "AWip9UrWnAiXIGWm5X2w6vaTBZYWCTl9EorYoz4I",
 });
 
 const bucket = new AWS.S3({
@@ -30,11 +30,11 @@ export const uploadDirectS3 = (
     .putObject(params)
     .on("httpUploadProgress", (evt) => {
       const progress = Math.round((evt.loaded / evt.total) * 100);
-      onProgress(progress)
-      alert("Uploading ... ("+progress+"/100)")
+      onProgress(progress);
     })
     .send((err) => {
       if (err) console.log(err);
+      alert("Upload completed");
       onComplete(err);
     });
 };
