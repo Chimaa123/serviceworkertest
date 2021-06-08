23d544e2f51c74cf346e2463c881ea71585d68a4
ChimaG <chimagee@mymusictaste.com>
2021-06-08 19:18:39 +0800

    fix: tmap undefined error

--- src/components/tmap.component.tsx
+++ src/components/tmap.component.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
-import {
-  getCurrentPosition,
-  watchLocation,
-} from "../services/location.service";
+import React, { useEffect } from "react";
+import { watchLocation } from "../services/location.service";
 import { Button } from "antd";
 
 declare global {
@@ -11,21 +8,18 @@ declare global {
   }
 }
 
-const height = window.innerHeight;
-
 const TMapComponent = () => {
   let map: any;
   let InfoWindow: any;
   let marker_s, marker_d;
-  const [currentLocation, setCurrentLocation] = useState();
 
   useEffect(() => {
-    getCurrentPosition(onLocationRetrieved);
     watchLocation(onLocationRetrieved);
     initTmap();
   }, []);
 
   function initTmap() {
+    console.log("initMAp", window.Tmapv2);
     // 1. 지도 띄우기
     map = new window.Tmapv2.Map("TMapApp", {
       center: new window.Tmapv2.LatLng(37.566481622437934, 126.98502302169841),
@@ -41,8 +35,28 @@ const TMapComponent = () => {
       position: new window.Tmapv2.LatLng(37.56445848334345, 127.00973587385866),
       icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_e.png",
       iconSize: new window.Tmapv2.Size(24, 38),
-      draggable: true,
       map: map,
+    });
+
+    marker_d.addListener("mouseenter", function (evt: any) {
+      InfoWindow = new window.Tmapv2.InfoWindow({
+        position: new window.Tmapv2.LatLng(
+          37.56445848334345,
+          127.00973587385866
+        ),
+        content:
+          "<div style=' position: relative; border-bottom: 1px solid #dcdcdc; line-height: 18px; padding: 0 35px 2px 0;'>" +
+          "<div style='font-size: 12px; line-height: 15px;'>" +
+          "<span style='display: inline-block; width: 40px; height: 14px; vertical-align: middle; margin-right: 5px;'></span>목적지" +
+          "</div>" +
+          "</div>",
+        type: 2,
+        map: map,
+      });
+    });
+    //Marker에 마우스가 마커를 벗어났을때 이벤트 등록.
+    marker_d.addListener("mouseleave", function (evt: any) {
+      InfoWindow.setVisible(false);
     });
   }
 
@@ -58,19 +72,26 @@ const TMapComponent = () => {
     if (geolocation == null) {
       return;
     }
-    const lat = 37.566481622437934; // geolocation.coords.latitude, // 위도
-    const lon = 126.98502302169841; //geolocation.coords.longitude; // 경도
+    const lat = geolocation.coords.latitude; // 위도
+    const lon = geolocation.coords.longitude; // 경도
     const position = new window.Tmapv2.LatLng(lat, lon);
     marker_s = new window.Tmapv2.Marker({
+      id: "currentLocation",
       position,
       draggable: true, //Marker의 드래그 가능 여부.
       icon: "http://tmapapi.sktelecom.com/upload/tmap/marker/pin_r_m_s.png",
       iconSize: new window.Tmapv2.Size(24, 38),
       map: map,
     });
+
     InfoWindow = new window.Tmapv2.InfoWindow({
       position: new window.Tmapv2.LatLng(lat, lon),
-      content: "<div>현재위치</div>",
+      content:
+        "<div style=' position: relative; border-bottom: 1px solid #dcdcdc; line-height: 18px; padding: 0 35px 2px 0;'>" +
+        "<div style='font-size: 12px; line-height: 15px;'>" +
+        "<span style='display: inline-block; width: 14px; height: 14px; background-image: url(/resources/images/common/icon_blet.png); vertical-align: middle; margin-right: 5px;'></span>현재위치" +
+        "</div>" +
+        "</div>",
       type: 2,
       map: map,
     });
@@ -79,18 +100,16 @@ const TMapComponent = () => {
 
   function changeMapType(type: "SATELLITE" | "HYBRID" | "ROAD") {
     console.log("changeMapType", type);
-    if ("SATELLITE" == type) {
+    if ("SATELLITE" === type) {
       map.setMapType(window.Tmapv2.Map.MapType.SATELLITE);
-    } else if ("HYBRID" == type) {
+    } else if ("HYBRID" === type) {
       map.setMapType(window.Tmapv2.Map.MapType.HYBRID);
-    } else if ("ROAD" == type) {
+    } else if ("ROAD" === type) {
       map.setMapType(window.Tmapv2.Map.MapType.ROAD);
     }
   }
 
-  function onClick() {
-    console.log("onclick on map");
-  }
+  function onClick() {}
 
   return (
     <div>
